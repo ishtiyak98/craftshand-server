531372686d7982e24d987dbcd231398bf59cb979
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-24 21:01:24 +0600

    make admin and verify admin route added

--- index.js
+++ index.js
@@ -157,6 +157,44 @@ async function run() {
       res.send(reviews);
     });
 
+
+    //!----------- get all users --------------
+    app.get("/users", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+
+    //!------------ Make an Admin --------------
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } 
+      else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    });
+
+    //!-------- Check Admin or Not ----------
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
   } finally {
     //await client.close();
   }
