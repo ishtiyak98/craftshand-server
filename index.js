2904c957aaea9182865b64777655d46386d16b10
ishtiyak98 <rifatishtiyak@gmail.com>
2022-05-24 14:09:52 +0600

    JWT token and my-order route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -15,6 +16,26 @@ app.get("/", (req, res) => {
   res.send("<center><h1>HandsCraft Server</h1></center>");
 });
 
+
+//!-------- Verify JWT Token ---------
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.4imeq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -27,6 +48,24 @@ async function run() {
     await client.connect();
     const toolsCollection = client.db("CraftsHand").collection("tools");
     const orderCollection = client.db("CraftsHand").collection("orders");
+    const userCollection = client.db("CraftsHand").collection("users");
+
+    //!-------- insert a user information to DB ---------
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const option = { upsert: true };
+      const updateUser = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateUser, option);
+      const token = jwt.sign({ email: email }, process.env.TOKEN_SECRET, {
+        expiresIn: "2hr",
+      });
+      res.send({ result, token });
+    });
+
 
     //!-------- get all Tools ---------
     app.get("/tools", async (req, res) => {
@@ -44,7 +83,6 @@ async function run() {
     });
 
     //!-------- update a tool quantity ---------
-
     app.put("/tools/:id", async (req, res) => {
       const id = req.params.id;
       const { name, image, description, minOrder, available, price } = req.body;
@@ -67,11 +105,29 @@ async function run() {
     });
 
     //!-------- Post an order tool ---------
-    app.post("/order", async (req, res) => {
+    app.post("/order", verifyJWT, async (req, res) => {
       const orderDetails = req.body;
       const output = await orderCollection.insertOne(orderDetails);
       res.send(output);
     });
+
+
+    //!-------- All My orders ---------
+    app.get("/order/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const output = await orderCollection.find(query).toArray();
+        res.send(output);
+      } 
+      else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+
   } finally {
     //await client.close();
   }
